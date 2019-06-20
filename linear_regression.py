fb0475981e86706bbdc2b01583e74800c954eca6
KacperKubara <kjk1u17@soton.ac.uk>
2019-06-20 21:42:14 +0100

    cost function added

--- linear_regression.py
+++ linear_regression.py
@@ -1,3 +1,4 @@
+import numpy as np 
 class LinearRegression():
     def _init_(self):
         pass
@@ -7,3 +8,7 @@ class LinearRegression():
 
     def predict(self, X):
         pass
+
+    def squared_error(self, y, y_pred):
+        sqr_err = (1/(2*np.size(y,0))) * np.sum(np.square(np.subtract(y,y_pred)))
+        return sqr_err
